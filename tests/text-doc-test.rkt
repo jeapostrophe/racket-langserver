30632c7341eabc3add85c4e6d7ca65c4a6a7fdaa
Conor Finegan <conor_finegan@student.uml.edu>
2018-02-06 11:00:38 -0500

    Removed tests that are out of sync with implementation.

--- tests/text-doc-test.rkt
+++ tests/text-doc-test.rkt
@@ -1,64 +1,8 @@
 #lang racket/base
+
 (require chk
-         delimit-app/main
          "../text-document.rkt")
 
-(define (test-did-open)
-  (define params
-    {'textDocument {'uri "/tmp/blank"
-                    'languageId "racket"
-                    'version 1
-                    'text "I am a banana!\r\nOne!\nTwo!!"}})
-  (chk (did-open (hasheq) params)
-       (hasheq '/tmp/blank '("I am a banana!" "One!" "Two!!"))))
-
-(define (test-did-close)
-  (define params {'textDocument {'uri "/tmp/blank"}})
-  (chk (did-close (hasheq '/tmp/blank '("placeholder" "text")) params)
-       (hasheq)))
-
-(define (test-did-change)
-  (define params
-    {'textDocument {'uri "/tmp/blank"
-                    'version 1}
-     'contentChanges [{'text "Some more text"}
-                      {'text "More text again\r\nNew line!"}]})
-  (chk (did-change (hasheq '/tmp/blank '("default" "text")) params)
-       (hasheq '/tmp/blank '("More text again" "New line!"))))
-
-(define (test-string->lines)
-  (chk (string->lines "a\nb") '("a" "b"))
-  (chk (string->lines "a\rb") '("a" "b"))
-  (chk (string->lines "a\r\nb") '("a" "b")))
-
-(define (test-range-edit)
-  (define test-lines '("line one" "line two" "line three" "line four" "line five"))
-  (chk (range-edit test-lines 1 5 2 4 "TWO\nLINE")
-       '("line one" "line TWO" "LINE three" "line four" "line five"))
-  (chk (range-edit test-lines 0 5 1 4 "ONE\nLINE")
-       '("line ONE" "LINE two" "line three" "line four" "line five"))
-  (chk (range-edit test-lines 1 5 3 4 "TWO\nLINE THREE\nLINE")
-       '("line one" "line TWO" "LINE THREE" "LINE four" "line five"))
-  (chk (range-edit test-lines 0 0 1 0 "LINE ONE\n")
-       '("LINE ONE" "line two" "line three" "line four" "line five"))
-  (chk (range-edit '("line one") 0 5 0 8 "ONE")
-       '("line ONE"))
-  ;; Unicode specific tests - '𐐀' (Deseret Capital Long Letter I) is used as an
-  ;; example of a unicode character which, when encoded as a UTF-16 string,
-  ;; requires 2 code points to represent.
-  (chk (range-edit '("abc" "wx𐐀yz" "asdf") 1 1 1 5 "NEW")
-       '("abc" "wNEWz" "asdf"))
-  ;; This is an invalid request, as the 'start-char' index is in the middle of
-  ;; the multi-code-point UTF-16 character. It is expected to return \uFFFD in
-  ;; place of the invalid char.
-  (chk #:t (for/or ([line (range-edit '("wx𐐀yz") 0 3 0 5 "NEW")])
-             (regexp-match? #rx"\uFFFD" line)))
-  )
-
 (module+ test
-  (test-did-open)
-  (test-did-close)
-  (test-did-change)
-  (test-string->lines)
-  (test-range-edit)
-  )
+  ;; TODO: new tests should mainly be about I/O, not the documents themselves.
+  (void))
