39d32bd25619a24b4e6a045114219f6e3df331f9
Conor Finegan <conor_finegan@student.uml.edu>
2018-03-01 15:05:58 -0500

    Added undeclared deps to info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,11 @@
 #lang info
 (define collection "racket-langserver")
-(define deps '("base"))
+(define deps '("base"
+               "compatibility-lib"
+               "data-lib"
+               "drracket-tool-lib"
+               "gui-lib"
+               "syntax-color-lib"))
 (define build-deps '("chk"))
 (define pkg-desc "Language Server Protocol implementation for Racket.")
 (define version "1.0")
