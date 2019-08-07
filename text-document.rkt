7cad741855cb31db4e36589e9631a3b8d86a5682
Daniel Rothfus <me@danielrothfus.com>
2019-08-06 20:28:48 -0700

    Add specialized logic for Windows resolution of file:// URIs

--- text-document.rkt
+++ text-document.rkt
@@ -24,7 +24,16 @@
   (string-prefix? str "file://"))
 
 (define (uri->path uri)
-  (substring uri 7))
+  (cond
+    [(eq? (system-type 'os) 'windows)
+     ;; If a file URI begins with file:// or file:////, Windows translates it
+     ;; as a UNC path. If it begins with file:///, it's translated to an MS-DOS
+     ;; path. (https://en.wikipedia.org/wiki/File_URI_scheme#Windows_2)
+     (cond
+       [(string-prefix? uri "file:////") (substring uri 7)]
+       [(string-prefix? uri "file:///") (substring uri 8)]
+       [else (string-append "//" (substring uri 7))])]
+    [else (substring uri 7)]))
 
 (define (abs-pos->Pos t pos)
   (define line (send t position-paragraph pos))
