a6314a4795f423d0cd22a9af4a21c410ed38a158
Keterr <kartelant@gmail.com>
2021-03-28 14:09:11 -0600

    fixes

--- check-syntax.rkt
+++ check-syntax.rkt
@@ -153,9 +153,10 @@
   (for ([lst (in-port (lexer-wrap lexer) in)] #:when (set-member? '(constant string symbol) (first (rest lst))))
     (match-define (list text type paren? start end) lst)
     (interval-map-set! symbols start end (list text type)))
-  ;; Rewind input port
-  (file-position in 0)
   
+  ;; Rewind input port
+  (set! in (open-input-string (send doc-text get-text)))
+  (port-count-lines! in)
   (define err-diags
     (parameterize ([current-annotations trace]
                    [current-namespace ns]
