6dd37b33d929f41547125d9e091053d214c0fd42
shhyou <shu-hung.you@eecs.northwestern.edu>
2023-07-20 22:24:56 -0500

    Refine package dependency

--- info.rkt
+++ info.rkt
@@ -3,13 +3,17 @@
 (define deps '("base"
                "compatibility-lib"
                "data-lib"
+               ;; Newer packages need only to depend on drracket-tool-text-lib.
+               ;; However, we still refer to drracket-tool-lib because it is split
+               ;; into the text- variant starting from Racket 8.4.
                "drracket-tool-lib"
                "gui-lib"
                "syntax-color-lib"
+               "sandbox-lib"  ;; running macro expansion with time limits
                "scribble-lib" ;; for blueboxes (scribble/blueboxes)
                "racket-index" ;; for cross references (setup/xref)
                "html-parsing" ;; for parsing documentation text
                ))
-(define build-deps '("chk"))
+(define build-deps '("chk-lib"))
 (define pkg-desc "Language Server Protocol implementation for Racket.")
 (define version "1.0")
