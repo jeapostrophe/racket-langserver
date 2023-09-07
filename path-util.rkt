f2ff33cb3ddecadd3f38849d767375f96ba97db7
6cdh <cd6cdh@gmail.com>
2023-09-07 09:06:54 +0800

    remove the limit that URI must be a file URI

--- path-util.rkt
+++ path-util.rkt
@@ -1,24 +1,12 @@
 #lang racket
 
 (provide path->uri
-         uri->path
-         uri-is-path?)
+         uri->path)
 
 (require net/url)
 
 (define path->uri (compose url->string path->url))
 
 (define (uri->path uri)
-  (cond
-    [(eq? (system-type 'os) 'windows)
-     ;; If a file URI begins with file:// or file:////, Windows translates it
-     ;; as a UNC path. If it begins with file:///, it's translated to an MS-DOS
-     ;; path. (https://en.wikipedia.org/wiki/File_URI_scheme#Windows_2)
-     (cond
-       [(string-prefix? uri "file:////") (substring uri 7)]
-       [(string-prefix? uri "file:///") (substring uri 8)]
-       [else (string-append "//" (substring uri 7))])]
-    [else (substring uri 7)]))
-
-(define (uri-is-path? str)
-  (string-prefix? str "file://"))
+  (cond [(string-prefix? uri "file:") (path->string (url->path (string->url uri)))]
+        [else (uri->path (regexp-replace #rx".*?:" uri "file:"))]))
