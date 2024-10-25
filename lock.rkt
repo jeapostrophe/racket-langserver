35baa32d677bd624e5d5e6ff4fdf6e49aa9f6e02
6cdh <cd6cdh@gmail.com>
2024-10-25 12:51:07 +0800

    fix: fix service interface and small style improvement

--- lock.rkt
+++ lock.rkt
@@ -2,33 +2,33 @@
 
 ;; A Readers Writer Lock
 (struct RWLock
-  (sema wsema readers)
+  (access-sema write-sema readers)
   #:mutable)
 
 (define (make-rwlock)
   (RWLock (make-semaphore 1) (make-semaphore 1) 0))
 
 (define (call-with-read-lock rwlock proc)
-  (define sema (RWLock-sema rwlock))
-  (define write (RWLock-wsema rwlock))
+  (define access (RWLock-access-sema rwlock))
+  (define write (RWLock-write-sema rwlock))
 
-  (semaphore-wait sema)
+  (semaphore-wait access)
   (when (= 0 (RWLock-readers rwlock))
     (semaphore-wait write))
   (set-RWLock-readers! rwlock (add1 (RWLock-readers rwlock)))
-  (semaphore-post sema)
+  (semaphore-post access)
 
   (define results (call-with-values proc list))
 
-  (semaphore-wait sema)
+  (semaphore-wait access)
   (set-RWLock-readers! rwlock (sub1 (RWLock-readers rwlock)))
   (when (= 0 (RWLock-readers rwlock))
     (semaphore-post write))
-  (semaphore-post sema)
+  (semaphore-post access)
   (apply values results))
 
 (define (call-with-write-lock rwlock proc)
-  (define write (RWLock-wsema rwlock))
+  (define write (RWLock-write-sema rwlock))
   (semaphore-wait write)
   (define results (call-with-values proc list))
   (semaphore-post write)
--- service/interface.rkt
+++ service/interface.rkt
@@ -12,24 +12,31 @@
     expand
     contract
     reset
-    walk))
+    walk-stx))
 
 (define base-service%
-  (class (annotations-mixin object%)
+  (class* (annotations-mixin object%)
+    (service<%> syncheck-annotations<%>)
+
     (super-new)
 
+    ;; return data
     (define/public (get)
       #f)
 
+    ;; insert text between from start to end (exclusive)
     (define/public (expand start end)
       (void))
 
+    ;; delete text between from start to end (exclusive)
     (define/public (contract start end)
       (void))
 
+    ;; clear all content
     (define/public (reset)
       (void))
 
+    ;; walk original syntax and expanded syntax
     (define/public (walk-stx stx expanded-stx)
       (void))))
 
