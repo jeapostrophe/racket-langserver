2a850a4eaee15e83a3488458700d85141209d614
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-08-28 12:59:19 +0800

    fetch resyntax enable option
    NOTE: need to find how to use ConfigurationItem, I'm not sure the
    meaning of scopeUri

--- settings.rkt
+++ settings.rkt
@@ -1,13 +1,25 @@
 #lang racket/base
-
 (provide get-resyntax-enabled
          set-resyntax-enabled!)
+(require "server-request.rkt"
+         "json-util.rkt")
+
+(define-json-expander ConfigurationItem
+  [scopeUri string?]
+  [section string?])
+
+(define (fetch-enable-resyntax)
+  (send-request 0 "workspace/configuration"
+    (hasheq 'items (list (ConfigurationItem #:scopeUri "racket-langserver" #:section "enable-resyntax")))
+    (λ (result)
+      (when (boolean? result)
+        (set-resyntax-enabled! result)))))
 
 (define resyntax-enabled? #t)
 
 (define (get-resyntax-enabled)
+  (fetch-enable-resyntax)
   resyntax-enabled?)
 
 (define (set-resyntax-enabled! val)
   (set! resyntax-enabled? val))
-
