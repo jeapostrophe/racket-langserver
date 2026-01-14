092e5c4805252e864d2b857936ef4e8c13f23622
6cdh <cd6cdh@gmail.com>
2026-01-14 21:46:12 +0800

    use parallel thread for heavy tasks in 9.0+

---
+++ version.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(provide version>=9.0?)
+
+(require version/utils)
+
+(define (version>=? target-version)
+  (not (version<? (version) target-version)))
+
+(define (version>=9.0?)
+  (version>=? "9.0"))
+
