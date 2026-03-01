baffe1af178d3241d36b0c7d46f2ca90e64c9168
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2026-03-01 14:03:56 +0800

    follows suggestions
    * separate global definitions
    * use `for*/set`

--- private/workspace.rkt
+++ private/workspace.rkt
@@ -5,23 +5,22 @@
          compiler/module-suffix)
 
 (define workspace-folders (mutable-set))
+
 (define (add-workspace-folder! path)
   (set-add! workspace-folders path))
+
 (define (remove-workspace-folder! path)
   (set-remove! workspace-folders path))
+
 (define (current-workspace-folders)
   (set->stream workspace-folders))
 
 (define (workspace-files)
-  (define files (mutable-set))
-
-  (for* ([dir (current-workspace-folders)]
-         [path (find-files
-                 (lambda (path) (for/or ([suffix (get-module-suffixes)]) (path-has-extension? path suffix)))
-                 dir)])
-    (set-add! files (path->complete-path path)))
-
-  files)
+  (for*/set ([dir (current-workspace-folders)]
+             [path (find-files
+                     (lambda (path) (for/or ([suffix (get-module-suffixes)]) (path-has-extension? path suffix)))
+                     dir)])
+    (path->complete-path path)))
 
 (provide current-workspace-folders
          add-workspace-folder!
